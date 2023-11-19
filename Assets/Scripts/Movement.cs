ce8c06f8c33b269c1106ebe383364484d4d545f7
Phantomforce260 <120600016+Phantomforce260@users.noreply.github.com>
2023-11-19 15:04:47 -0500

    Physics changes

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-  public Rigidbody2D rb;
+  private Rigidbody2D rb;
   public float jumpStrength = 5000;
   public bool jumped = false;
   public Vector2 velocity = new Vector2(2, 2);
@@ -14,7 +14,7 @@ public class Movement : MonoBehaviour
 
   void Start()
   {
-
+        rb = GetComponent<Rigidbody2D>();
   }
 
   void Update()
@@ -24,8 +24,8 @@ public class Movement : MonoBehaviour
 
   void UpdateControls()
   {
-    // Enes (the goat's code)
-    rb.AddForce(Vector2.right * speed);
+        // Enes (the goat's code)
+    rb.velocity = new Vector2(speed, rb.velocity.y);
 
     if (Input.GetAxisRaw("Jump") != 0 && !jumped)
     {
