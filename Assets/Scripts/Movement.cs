db0731b0ea9af25f0e4c8574a1727c051656e099
Harrison DiAmbrosio <haranaitor@gmail.com>
2023-11-20 20:44:44 -0500

    Added animation to the spites introduced by Adm the goat <3

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -5,33 +5,50 @@ using UnityEngine;
 public class Movement : MonoBehaviour
 {
   private Rigidbody2D rb;
-  public float jumpStrength = 5000;
-  public bool jumped = false;
-  public Vector2 velocity = new Vector2(2, 2);
-  public Vector2 position = new Vector2(0, 0);
 
+  public float jumpStrength = 5000;
+  private bool jumped = false;
+  public Sprite[] sprites = new Sprite[3];
+  private SpriteRenderer spriteRenderer;
+
+  private float startTime;
   public float speed = 16f; 
+
+  public float TIME_FOR_ALL_FRAMES;
 
   void Start()
   {
-        rb = GetComponent<Rigidbody2D>();
+    rb = GetComponent<Rigidbody2D>();
+    spriteRenderer= GetComponent<SpriteRenderer>();
+
+    startTime = Time.realtimeSinceStartup;
   }
 
   void Update()
   {
     UpdateControls();
+    UpdateSprite();
   }
 
   void UpdateControls()
   {
-        // Enes (the goat's code)
-    rb.velocity = new Vector2(speed, rb.velocity.y);
+    // rb.velocity = new Vector2(speed, rb.velocity.y);
 
     if (Input.GetAxisRaw("Jump") != 0 && !jumped)
     {
       Debug.Log(Input.GetAxisRaw("Jump") == 1);
       rb.AddForce(new Vector2(0, jumpStrength));
       jumped = true;
+    }
+  }
+
+  void UpdateSprite() {
+    int spriteIndex = (int) Mathf.Floor(((Time.realtimeSinceStartup % TIME_FOR_ALL_FRAMES) / TIME_FOR_ALL_FRAMES * 3));
+
+    if (rb.velocity.sqrMagnitude == 0) {
+      spriteRenderer.sprite = sprites[spriteIndex];
+    } else if(rb.velocity.sqrMagnitude != 0) {
+
     }
   }
 
