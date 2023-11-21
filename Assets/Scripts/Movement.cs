e7836810fe57cbea5c55ee372bfce871aba90636
Harrison DiAmbrosio <haranaitor@gmail.com>
2023-11-20 21:24:59 -0500

    Added sprite reversal and code comments for movement <3

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -6,22 +6,20 @@ public class Movement : MonoBehaviour
 {
   private Rigidbody2D rb;
 
-  public float jumpStrength = 5000;
+  public float jumpStrength = 2500;
   private bool jumped = false;
+  public float speed = 4.0f; 
+
   public Sprite[] sprites = new Sprite[3];
   private SpriteRenderer spriteRenderer;
 
-  private float startTime;
-  public float speed = 16f; 
-
-  public float TIME_FOR_ALL_FRAMES;
+  // Time it takes to cycle all the frames in seconds
+  public float frameCycleTime = 2;
 
   void Start()
   {
     rb = GetComponent<Rigidbody2D>();
     spriteRenderer= GetComponent<SpriteRenderer>();
-
-    startTime = Time.realtimeSinceStartup;
   }
 
   void Update()
@@ -32,7 +30,7 @@ public class Movement : MonoBehaviour
 
   void UpdateControls()
   {
-    // rb.velocity = new Vector2(speed, rb.velocity.y);
+    rb.velocity = new Vector2(speed, rb.velocity.y);
 
     if (Input.GetAxisRaw("Jump") != 0 && !jumped)
     {
@@ -43,21 +41,27 @@ public class Movement : MonoBehaviour
   }
 
   void UpdateSprite() {
-    int spriteIndex = (int) Mathf.Floor(((Time.realtimeSinceStartup % TIME_FOR_ALL_FRAMES) / TIME_FOR_ALL_FRAMES * 3));
-
-    if (rb.velocity.sqrMagnitude == 0) {
+    // Cycles the index from 0 to the number of frames and repeats
+    float animationProgress = Time.realtimeSinceStartup % frameCycleTime / frameCycleTime;
+    int spriteIndex = (int) Mathf.Floor(animationProgress * sprites.Length);
+    if (rb.velocity.sqrMagnitude != 0) {
       spriteRenderer.sprite = sprites[spriteIndex];
-    } else if(rb.velocity.sqrMagnitude != 0) {
+    }
 
+    // Mirror the sprite on the y if its movement doesnt match it's sprite direction
+    if((rb.velocity.x > 0 && transform.localScale.x < 0) || (rb.velocity.x < 0 && transform.localScale.x > 0)) {
+      transform.localScale = new Vector3(transform.localScale.x * -1, transform.localScale.y, transform.localScale.z);
     }
   }
 
   void OnCollisionEnter2D(Collision2D coll)
   {
+    // If the player collides with a gmae object tagged "ground" let it jump again
     if(coll.gameObject.tag == "Ground") {
       jumped = false;
     }
 
+    // Reverse the x speed if player collides witha wall and give it a push in the new direction
     if(coll.gameObject.tag == "Wall") {
       speed *= -1;
       rb.AddForce(Vector2.right * speed, ForceMode2D.Impulse);
