e5621d8b2b2dd260a85604b9e5f65ddfa2412559
Slys <slysdeveloper@gmail.com>
2023-11-26 15:01:59 -0500

    Remove increasing fireball scale on charge
    This commit removes the coroutine that increases the size of the fireball the longer the player charges the spell.

--- Assets/Scripts/SpellCasting.cs
+++ Assets/Scripts/SpellCasting.cs
@@ -21,7 +21,6 @@ public class SpellCasting : MonoBehaviour
         // Confirm that we can shoot
         if (Input.GetButtonDown("Fire1") && Movement.spellReady && shootCounter == 0)
         {
-            StartCoroutine(IncreaseFireballSizeCoroutine());
             shootCounter += 1;
         }
 
@@ -36,24 +35,5 @@ public class SpellCasting : MonoBehaviour
     void Shoot(float fireballSize) {
         // Shooting Logic with a specified fireball size
         GameObject newFireball = Instantiate(fireBallPrefab, firePoint.position, firePoint.rotation);
-        newFireball.transform.localScale = new Vector2(fireballSize, fireballSize);
     }
-    IEnumerator IncreaseFireballSizeCoroutine() {
-        float currentFireballSize = 0.2f;
-
-        // Increase the size as long as the fire button is held and the size is below the maximum
-        while (Input.GetButton("Fire1") && currentFireballSize < maxFireballSize)
-        {
-            currentFireballSize += sizeIncreaseRate * Time.deltaTime;
-            yield return null;
-        }
-
-        // Shoot the fireball with the final size
-        if (Movement.spellReady)
-        {
-            Shoot(currentFireballSize);
-        }
-        
-    }
-
 }
